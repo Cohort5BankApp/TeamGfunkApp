d0c585485391d87d80ffefeafbd8ee0ddceed73b
Avonte Cannon <acannon007@my.wilmu.edu>
2019-06-03 16:10:52 -0400

    changed controller

--- src/main/java/com/william/fullbankingapplicationfinal/controller/BillContoller.java
+++ src/main/java/com/william/fullbankingapplicationfinal/controller/BillContoller.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -35,21 +36,52 @@ public class BillContoller {
         return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 
+
+
     //Get A Bill by ID
-    @RequestMapping(value = "/api/bills/{id}", method = RequestMethod.GET)
-    public Optional<Bill> getBillRecord(@PathVariable Long id) {
-        return billService.getBillById(id);
+    @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.GET)
+    public Optional<Bill> getBillRecord(@PathVariable Long id)
+        if (billRepository.findById(bill).isPresent()){
+            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "error fetch bill with id");
+            {
+                return billService.getBillById(id);
+            }
+        }
+
+        //Get all Bills by specific Account
+        @RequestMapping(value = "accounts/{account_id}/bills", method = RequestMethod.GET)
+        public Iterable<Bill> getBillByAccountIdRecord (@PathVariable Long id)
+        if(billRepository.findById(bill).isPresent()){
+            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "error fetching bills");
+            { return billService.getBillByAccountId(id);
+        }
     }
 
-    //Delete Bill
-    @RequestMapping(value = "/api/bills/{id}", method = RequestMethod.DELETE)
-    public void deleteBillRecord(@PathVariable Long id) {
-        billService.deleteBill(id);
+        //Get all Customer Bills
+        @RequestMapping(value = "customers/{customers_id}/bills", method = RequestMethod.GET)
+        public Iterable<Bill> getBillByAccountIdRecord (@PathVariable Long id)
+        if (billRepository.findById(bill).isPresent()){
+            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "error fetching bills");
+            { return billService.getBillByAccountId(id);
+        }
     }
 
-    //Update Bill
-    @RequestMapping(value = "/api/bills/{id}", method = RequestMethod.PUT)
-    public void updateBillRecord(@RequestBody Bill bill, @PathVariable Long id) {
-        billService.updateBill(bill);
+        //Delete Bill
+        @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.DELETE)
+        public void deleteBillRecord (@PathVariable Long id)
+        if(billRepository.findById(bill).isPresent()){
+        throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "This ID does not exist in bills");
+            { billService.deleteBill(id);
+        }
+        }
+
+        //Update Bill
+        @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.PUT)
+        public void updateBillRecord (@RequestBody Bill bill, @PathVariable Long id)
+        if(billRepository.findById(bill).isPresent()){
+            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "Bill ID does not exist");
+            { billService.updateBill(bill);
+        }
     }
 }
+
