57a3cbe0d91d58d0781522d1c5e00ff44239b042
Alberto Rodriguez <ararod9@gmail.com>
2019-06-03 17:05:57 -0400

    second commit

--- src/main/java/com/william/fullbankingapplicationfinal/controller/DepositController.java
+++ src/main/java/com/william/fullbankingapplicationfinal/controller/DepositController.java
@@ -4,14 +4,18 @@ package com.william.fullbankingapplicationfinal.controller;
 import com.william.fullbankingapplicationfinal.model.Deposit;
 import com.william.fullbankingapplicationfinal.repository.DepositRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
+import java.nio.charset.Charset;
 import java.util.Optional;
 
 @RestController
 public class DepositController {
+
 
 	@Autowired
 	private DepositRepository depositRepository;
@@ -19,30 +23,49 @@ public class DepositController {
 	@RequestMapping(path = "/accounts/{accountId}/deposits",method = RequestMethod.GET)
 	public ResponseEntity<Iterable<Deposit>> getAllDeposit(){
 		Iterable<Deposit> AllDeposit = depositRepository.findAll();
+		if(null == AllDeposit){
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Account not found");
+		}
+
 		return new ResponseEntity<>(depositRepository.findAll(), HttpStatus.OK);
 	}
 
 	@RequestMapping(path = "/deposits/{depositId}", method = RequestMethod.GET)
 	public ResponseEntity<Optional<Deposit>> getDeposit(@PathVariable Long id){
+		if (null== getDeposit(id)){
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"error fetching deposit with id");
+		}
+
 		return new ResponseEntity<>(depositRepository.findById(id), HttpStatus.OK);
 	}
 
 	@RequestMapping(path = "/accounts/{accountId}/deposits", method = RequestMethod.POST)
 	public ResponseEntity<?> createDeposit(@RequestBody Deposit deposit){
-		depositRepository.save(deposit);
+		Deposit d = depositRepository.save(deposit);
+
+		if (null == d){
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Error creating deposit: Account not found");
+		}
 		return new ResponseEntity<>(null, HttpStatus.CREATED);
 	}
 
 	@RequestMapping(path = "/deposits/{depositId}",method = RequestMethod.PUT)
 	public ResponseEntity<?> updateDeposit(@RequestBody Deposit deposit, @PathVariable Long id){
-		depositRepository.save(deposit);
-		return new ResponseEntity<>(HttpStatus.OK);
+		Deposit d = depositRepository.save(deposit);
+
+		if(null == d){
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Deposit ID does not exist");
+		}
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
 	}
 
 	@RequestMapping(path = "/deposits/{depositId}",method = RequestMethod.DELETE)
 		public ResponseEntity<?> deleteDeposit(@PathVariable Long id){
 		depositRepository.deleteById(id);
-		return new ResponseEntity<>(HttpStatus.OK);
+		if(null == id){
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"This id does not exist in deposits");
+		}
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 }
