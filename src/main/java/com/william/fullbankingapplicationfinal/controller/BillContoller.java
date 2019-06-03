a7abda584f8ccf8e3dec5b24644be30cd5cfefcc
Avonte Cannon <acannon007@my.wilmu.edu>
2019-06-03 17:09:54 -0400

    updated controller

--- src/main/java/com/william/fullbankingapplicationfinal/controller/BillContoller.java
+++ src/main/java/com/william/fullbankingapplicationfinal/controller/BillContoller.java
@@ -41,47 +41,35 @@ public class BillContoller {
     //Get A Bill by ID
     @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.GET)
     public Optional<Bill> getBillRecord(@PathVariable Long id)
-        if (billRepository.findById(bill).isPresent()){
-            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "error fetch bill with id");
-            {
-                return billService.getBillById(id);
+            { return billService.getBillById(id);
             }
-        }
+
 
         //Get all Bills by specific Account
         @RequestMapping(value = "accounts/{account_id}/bills", method = RequestMethod.GET)
         public Iterable<Bill> getBillByAccountIdRecord (@PathVariable Long id)
-        if(billRepository.findById(bill).isPresent()){
-            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "error fetching bills");
             { return billService.getBillByAccountId(id);
         }
-    }
+
 
         //Get all Customer Bills
         @RequestMapping(value = "customers/{customers_id}/bills", method = RequestMethod.GET)
         public Iterable<Bill> getBillByAccountIdRecord (@PathVariable Long id)
-        if (billRepository.findById(bill).isPresent()){
-            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "error fetching bills");
             { return billService.getBillByAccountId(id);
         }
-    }
+
 
         //Delete Bill
         @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.DELETE)
         public void deleteBillRecord (@PathVariable Long id)
-        if(billRepository.findById(bill).isPresent()){
-        throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "This ID does not exist in bills");
             { billService.deleteBill(id);
-        }
         }
 
         //Update Bill
         @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.PUT)
         public void updateBillRecord (@RequestBody Bill bill, @PathVariable Long id)
-        if(billRepository.findById(bill).isPresent()){
-            throw new HttpClientErrorException(HttpStatus.CONFLICT, bill + "Bill ID does not exist");
             { billService.updateBill(bill);
         }
     }
-}
+
 
