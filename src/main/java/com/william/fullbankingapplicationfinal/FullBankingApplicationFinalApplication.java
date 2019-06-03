834de8315607ee0028685aa7999ba0314b3afc4b
billycropper <williamcropper@Billys-MacBook-Pro.local>
2019-06-03 14:31:20 -0400

    project start

---
+++ src/main/java/com/william/fullbankingapplicationfinal/FullBankingApplicationFinalApplication.java
@@ -0,0 +1,13 @@
+package com.william.fullbankingapplicationfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullBankingApplicationFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullBankingApplicationFinalApplication.class, args);
+	}
+
+}
