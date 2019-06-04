5307c04a485fe178e7829eda0c845eade47c2636
billycropper <williamcropper@Billys-MacBook-Pro.local>
2019-06-04 09:41:33 -0400

    updated bill service

--- src/main/java/com/william/fullbankingapplicationfinal/repository/AccountRepository.java
+++ src/main/java/com/william/fullbankingapplicationfinal/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.william.fullbankingapplicationfinal.repository;
 
+import com.william.fullbankingapplicationfinal.model.Account;
+import com.william.fullbankingapplicationfinal.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<Customer, Long> {
+public interface AccountRepository extends CrudRepository<Account, Long> {
 }
