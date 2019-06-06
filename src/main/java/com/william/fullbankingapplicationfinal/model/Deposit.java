853dce59071770b15298fda66f308f268bc29701
billycropper <williamcropper@Billys-MBP.local>
2019-06-05 20:43:07 -0400

    commit

--- src/main/java/com/william/fullbankingapplicationfinal/model/Deposit.java
+++ src/main/java/com/william/fullbankingapplicationfinal/model/Deposit.java
@@ -1,48 +1,61 @@
 package com.william.fullbankingapplicationfinal.model;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
+import javax.persistence.*;
 
-@Embeddable
+//@Embeddable
+@Entity
 public class Deposit {
 
-	public Deposit(DepositStatus status, DepositMedium medium, DepositType type ){
-		this.status = status;
-		this.medium = medium;
+//	public Deposit(DepositStatus status, DepositMedium medium, DepositType type ){
+//		this.status = status;
+//		this.medium = medium;
+//		this.type = type;
+//	}
+
+	public Deposit(Long id, DepositType type, String transaction_date, DepositStatus status, Long account_id, DepositMedium medium, Double amount, String description) {
+		this.id = id;
 		this.type = type;
+		this.transaction_date = transaction_date;
+		this.status = status;
+		this.account_id = account_id;
+		this.medium = medium;
+		this.amount = amount;
+		this.description = description;
 	}
 
-	@Column(name = "Deposit_id")
+	@Id
+	@GeneratedValue
 	private Long id;
 
-	@Column(name = "Type_transaction")
 	private DepositType type;
 	private enum DepositType{
 		P2P, DEPOSIT, WITHDRAWAL;
 	}
 
-	@Column(name = "Timestamp_execution")
+	//@Column(name = "Timestamp_execution")
 	private String transaction_date;
 
-	@Column(name = "Deposit_Status")
+	//@Column(name = "Deposit_Status")
+	//@Embedded
 	private DepositStatus status;
 	public enum DepositStatus{
 		PENDING, CANCELLED, COMPLETED;
 	}
 
-	@Column(name = "Account_id")
+	//@Column(name = "Account_id")
 	private Long account_id;
 
-	@Column(name = "Deposit_medium")
+	//@Column(name = "Deposit_medium")
+	//@Embedded
 	private DepositMedium medium;
 	public enum DepositMedium{
 		BALANCE, REWARDS
 	}
 
-	@Column(name = "Deposit_amount")
+	//@Column(name = "Deposit_amount")
 	private Double amount;
 
-	@Column(name = "Description")
+	//@Column(name = "Description")
 	private String description;
 
 	public Long getId() {
@@ -107,5 +120,19 @@ public class Deposit {
 
 	public void setMedium(DepositMedium medium) {
 		this.medium = medium;
+	}
+
+	@Override
+	public String toString() {
+		return "Deposit{" +
+				"id=" + id +
+				", type=" + type +
+				", transaction_date='" + transaction_date + '\'' +
+				", status=" + status +
+				", account_id=" + account_id +
+				", medium=" + medium +
+				", amount=" + amount +
+				", description='" + description + '\'' +
+				'}';
 	}
 }
--- src/main/java/com/william/fullbankingapplicationfinal/model/Withdrawal.java
+++ src/main/java/com/william/fullbankingapplicationfinal/model/Withdrawal.java
@@ -1,47 +1,60 @@
 package com.william.fullbankingapplicationfinal.model;
 
-import javax.persistence.Column;
+//import org.springframework.data.annotation.Id;
 
+import javax.persistence.Id;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class Withdrawal {
 
-	public Withdrawal(WithdrawalStatus status, WithdrawalMedium medium, WithdrawalType type ){
-		this.status = status;
-		this.medium = medium;
+	public Withdrawal(Long id, WithdrawalType type, String transaction_date, WithdrawalStatus status, Long account_id, WithdrawalMedium medium, Double amount, String description) {
+		this.id = id;
 		this.type = type;
+		this.transaction_date = transaction_date;
+		this.status = status;
+		this.account_id = account_id;
+		this.medium = medium;
+		this.amount = amount;
+		this.description = description;
 	}
 
-	@Column(name = "Deposit_id")
+	@Id
+	@GeneratedValue
 	private Long id;
 
-	@Column(name = "Type_transaction")
+	//@Column(name = "Type_transaction")
 	private WithdrawalType type;
 	private enum WithdrawalType{
 		P2P,DEPOSIT,WITHDRAWAL;
 	}
 
-	@Column(name = "Timestamp_execution")
+	//@Column(name = "Timestamp_execution")
 	private String transaction_date;
 
-	@Column(name = "Deposit_Status")
+	//@Column(name = "Deposit_Status")
 	private WithdrawalStatus status;
 	public enum WithdrawalStatus{
 		PENDING,CANCELLED,COMPLETED;
 	}
 
-	@Column(name = "Account_id")
+	//@Column(name = "Account_id")
 	private Long account_id;
 
-	@Column(name = "Deposit_medium")
+	//@Column(name = "Deposit_medium")
 	private WithdrawalMedium medium;
 	public enum WithdrawalMedium{
 		BALANCE,REWARDS
 	}
 
-	@Column(name = "Deposit_amount")
+	//@Column(name = "Deposit_amount")
 	private Double amount;
 
-	@Column(name = "Description")
+	//@Column(name = "Description")
 	private String description;
+
 
 	public Long getId() {
 		return id;
@@ -51,6 +64,13 @@ public class Withdrawal {
 		this.id = id;
 	}
 
+	public WithdrawalType getType() {
+		return type;
+	}
+
+	public void setType(WithdrawalType type) {
+		this.type = type;
+	}
 
 	public String getTransaction_date() {
 		return transaction_date;
@@ -60,12 +80,28 @@ public class Withdrawal {
 		this.transaction_date = transaction_date;
 	}
 
+	public WithdrawalStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(WithdrawalStatus status) {
+		this.status = status;
+	}
+
 	public Long getAccount_id() {
 		return account_id;
 	}
 
 	public void setAccount_id(Long account_id) {
 		this.account_id = account_id;
+	}
+
+	public WithdrawalMedium getMedium() {
+		return medium;
+	}
+
+	public void setMedium(WithdrawalMedium medium) {
+		this.medium = medium;
 	}
 
 	public Double getAmount() {
@@ -84,27 +120,17 @@ public class Withdrawal {
 		this.description = description;
 	}
 
-	public WithdrawalType getType() {
-		return type;
-	}
-
-	public void setType(WithdrawalType type) {
-		this.type = type;
-	}
-
-	public WithdrawalStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(WithdrawalStatus status) {
-		this.status = status;
-	}
-
-	public WithdrawalMedium getMedium() {
-		return medium;
-	}
-
-	public void setMedium(WithdrawalMedium medium) {
-		this.medium = medium;
+	@Override
+	public String toString() {
+		return "Withdrawal{" +
+				"id=" + id +
+				", type=" + type +
+				", transaction_date='" + transaction_date + '\'' +
+				", status=" + status +
+				", account_id=" + account_id +
+				", medium=" + medium +
+				", amount=" + amount +
+				", description='" + description + '\'' +
+				'}';
 	}
 }
